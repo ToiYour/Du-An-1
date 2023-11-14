84efb88aa26ee605a024f47537483151d5750309
ToiYour <toidz25102004@gmail.com>
2023-11-14 07:06:53 +0700

    #7-toinhph33994 update AdminHome

--- assets/dao/don-hang.php
+++ assets/dao/don-hang.php
@@ -1,6 +1,11 @@
 <?php
 require_once 'pdo.php';
 include_once 'toast-message.php';
+function don_hang_count()
+{
+    $sql = "SELECT COUNT(1) FROM don_hang";
+    return pdo_query_count($sql);
+}
 function total_price($id_don_hang)
 {
     $sql = "SELECT chi_tiet_don_hang.id_don_hang, SUM(chi_tiet_san_pham.gia_ban * chi_tiet_don_hang.so_luong) AS total_price FROM chi_tiet_don_hang JOIN chi_tiet_san_pham ON chi_tiet_don_hang.id_chi_tiet_san_pham = chi_tiet_san_pham.id_chi_tiet_san_pham WHERE chi_tiet_don_hang.id_don_hang = ? GROUP BY chi_tiet_don_hang.id_don_hang, chi_tiet_san_pham.id_chi_tiet_san_pham";
@@ -25,4 +30,50 @@ function don_hang_detail($id_don_hang)
 {
     $sql = "SELECT chi_tiet_don_hang.id_chi_tiet_don_hang,san_pham.ten_san_pham,chi_tiet_don_hang.so_luong,chi_tiet_san_pham.gia_ban,size.kich_thuoc,size.size,mau.mau,SUM(chi_tiet_don_hang.so_luong * chi_tiet_san_pham.gia_ban) AS 'total_price' FROM `chi_tiet_don_hang` JOIN chi_tiet_san_pham ON chi_tiet_don_hang.id_chi_tiet_san_pham = chi_tiet_san_pham.id_chi_tiet_san_pham JOIN size ON chi_tiet_san_pham.id_size = size.id_size JOIN mau ON chi_tiet_san_pham.id_mau = mau.id_mau JOIN san_pham ON chi_tiet_san_pham.id_san_pham = san_pham.id_san_pham WHERE id_don_hang = ? GROUP BY chi_tiet_don_hang.id_chi_tiet_don_hang,san_pham.ten_san_pham,chi_tiet_don_hang.so_luong,chi_tiet_san_pham.gia_ban,size.kich_thuoc,mau.mau;";
     return pdo_query($sql, $id_don_hang);
+}
+function don_hang_thong_ke_home()
+{
+    $sql = "SELECT SUM(chi_tiet_don_hang.so_luong) AS total_dh, SUM(chi_tiet_don_hang.so_luong * chi_tiet_san_pham.gia_ban) AS total_price, AVG( chi_tiet_san_pham.gia_ban) AS avg_price FROM `don_hang` JOIN chi_tiet_don_hang ON don_hang.id_don_hang = chi_tiet_don_hang.id_don_hang JOIN chi_tiet_san_pham ON chi_tiet_don_hang.id_chi_tiet_san_pham = chi_tiet_san_pham.id_chi_tiet_san_pham WHERE don_hang.id_trang_thai_don >= 2";
+    return pdo_query_one($sql);
+}
+function don_hang_history_home()
+{
+    $sql = "SELECT
+    chi_tiet_don_hang.id_chi_tiet_don_hang,
+    san_pham.ten_san_pham,
+    user.ho_ten,
+    danh_muc.ten_danh_muc,
+    don_hang.id_don_hang,
+    chi_tiet_don_hang.so_luong,
+    chi_tiet_san_pham.gia_ban,
+    size.kich_thuoc,
+    size.size,
+    mau.mau,
+    don_hang.id_trang_thai_don,
+    SUM(chi_tiet_don_hang.so_luong * chi_tiet_san_pham.gia_ban) AS 'total_price',
+    AVG(chi_tiet_don_hang.so_luong * chi_tiet_san_pham.gia_ban) AS 'avg_price'
+FROM
+    `chi_tiet_don_hang`
+JOIN chi_tiet_san_pham ON chi_tiet_don_hang.id_chi_tiet_san_pham = chi_tiet_san_pham.id_chi_tiet_san_pham
+JOIN size ON chi_tiet_san_pham.id_size = size.id_size
+JOIN don_hang ON chi_tiet_don_hang.id_don_hang = don_hang.id_don_hang  
+JOIN user ON don_hang.id_kh = user.id_kh
+JOIN san_pham ON chi_tiet_san_pham.id_san_pham = san_pham.id_san_pham
+JOIN danh_muc ON san_pham.id_danh_muc = danh_muc.id_danh_muc
+JOIN mau ON chi_tiet_san_pham.id_mau = mau.id_mau
+JOIN trang_thai_don ON don_hang.id_trang_thai_don = trang_thai_don.id_trang_thai_don
+GROUP BY
+     chi_tiet_don_hang.id_chi_tiet_don_hang,
+    san_pham.ten_san_pham,
+    user.ho_ten,
+    danh_muc.ten_danh_muc,
+    don_hang.id_don_hang,
+    chi_tiet_don_hang.so_luong,
+    chi_tiet_san_pham.gia_ban,
+    size.kich_thuoc,
+    size.size,
+    mau.mau,
+    don_hang.id_trang_thai_don
+";
+    return pdo_query($sql);
 }
